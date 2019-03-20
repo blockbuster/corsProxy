7afc5d234dcbfa033002b22cf56695184b3c5fcc
freko247 <freko247@gmail.com>
2019-03-20 10:02:45 +0100

    Linting

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
-const utils = require('./utils.js')
+const utils = require('./utils.js');
 
 exports.handler = async (event, context) => {
-  const res = await utils.sendServiceRequest(event)
-  console.log(JSON.stringify(res.data))
+  const res = await utils.sendServiceRequest(event);
+  console.log(JSON.stringify(res.data));
   return {
-      headers: res.headers,
-      statusCode: res.statusCode,
-      body: JSON.stringify(res.data)
-  }
-}
+    headers: res.headers,
+    statusCode: res.statusCode,
+    body: JSON.stringify(res.data)
+  };
+};
