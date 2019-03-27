084e1c8118f864fd5cbd2fd2652292388e5e3df6
Feisal <iam@feisal.dk>
2019-03-27 12:48:42 +0100

    access control

--- app/app.js
+++ app/app.js
@@ -3,6 +3,9 @@ const utils = require('./utils.js');
 exports.handler = async (event) => {
   const res = await utils.sendServiceRequest(event);
   console.log(JSON.stringify(res.data));
+
+  const origin = event.headers.origin||event.headers.Origin;
+  res.headers['access-control-allow-origin'] = origin;
   return {
     headers: res.headers,
     statusCode: res.statusCode,
