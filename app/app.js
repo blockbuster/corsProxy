d2526c47f0d69fdbd58c2d6fa555e4ed8d1d6fb4
Feisal <iam@feisal.dk>
2019-03-28 11:03:49 +0100

    added option method

--- app/app.js
+++ app/app.js
@@ -2,13 +2,30 @@ const utils = require('./utils.js');
 
 exports.handler = async (event) => {
   const res = await utils.sendServiceRequest(event);
-  console.log(JSON.stringify(res.data));
-
+  const method = event.requestContext.httpMethod;
   const origin = event.headers.origin||event.headers.Origin;
   res.headers['access-control-allow-origin'] = origin;
+
+  res.headers['transfer-encoding'] =  '';
+  if (method === 'OPTIONS'){
+    res.statusCode = 200;
+    res.headers['access-control-allow-origin'] = origin;
+  }
   return {
     headers: res.headers,
     statusCode: res.statusCode,
     body: JSON.stringify(res.data)
+  };
+};
+
+
+
+exports.option = async (event) => {
+  const origin = event.headers.origin||event.headers.Origin;
+  const headers = event.headers;
+  headers['access-control-allow-origin'] = origin;
+  return {
+    headers: headers,
+    statusCode: 200,
   };
 };
